7420e2254cc97a81bf133d5715eaac831eb62b3c
BrunoVigel <brunorosav@gmail.com>
2022-07-07 14:14:23 -0300

    first commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+//Importa o Head e o Html aqui
+import Document, { Main, NextScript, Head, Html } from "next/document";
+
+export default class MyDocument extends Document {
+  
+  // Utiliza eles dentro do render no lugar dos elementos nativos
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,500;0,700;1,400&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
